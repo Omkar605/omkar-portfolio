d6596f79784a89d8a96a7ae5125c079aa750c39e
Omkar Vijay Zagade <omkar.zagade@i-exceed.com>
2025-02-27 00:03:28 +0530

    Fixed missing route issue

--- portfolio-backend/server.js
+++ portfolio-backend/server.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("Backend is running successfully!");
+});
+
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
