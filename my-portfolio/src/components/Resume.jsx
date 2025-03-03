49eb52642423f8478039b6612ed489f06b8146dc
Omkar Vijay Zagade <omkar.zagade@i-exceed.com>
2025-03-03 13:18:08 +0530

    resume updated

--- my-portfolio/src/components/Resume.jsx
+++ my-portfolio/src/components/Resume.jsx
@@ -19,7 +19,7 @@ const Resume = () => {
       </div>
       <a
         href={sampleResume}
-        download="Resume.pdf"
+        download="resume.pdf"
         className="mt-4 bg-blue-600 hover:bg-blue-800 px-4 py-2 rounded"
       >
         Download Resume
