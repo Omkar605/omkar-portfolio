4480bdf90c32f29e92b468bf39a45d604fe2b532
Omkar Zagade <omkar.zagade@i-exceed.com>
2025-09-23 22:40:05 +0530

    about updated

--- my-portfolio/src/components/About.jsx
+++ my-portfolio/src/components/About.jsx
@@ -3,10 +3,7 @@ const About = () => {
     <section id="about" className="min-h-screen bg-gray-800 text-white flex flex-col justify-center items-center">
       <h2 className="text-4xl font-bold">About Me</h2>
       <p className="max-w-2xl mt-4 text-center">
-       {`I'm a passionate Frontend Developer with nearly 3 years of experience crafting responsive and secure web interfaces.
-      I specialize in building scalable and accessible applications using modern technologies like React.js, JavaScript,
-      HTML, and CSS. In my current role, I've contributed to features across various modules including Deposits, Loans,
-      Pre-login flows, Security Q&A, VAPT, and Sonar issue resolution.`}
+       {`Full-stack developer with 3 years of experience in building scalable, user-focused web applications using React.js and Java. Proficient in developing RESTful APIs using Spring Boot and building microservice-based backend architecture. Skilled in database design and management with PostgreSQL and MongoDB. Passionate about learning new technologies and improving coding skills through continuous practice and collaboration.`}
       </p>
     </section>
   );
